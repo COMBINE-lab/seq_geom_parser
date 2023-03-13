1434659d875b2c252ae479a7ae8e6f97c02e8b94
Robert Patro <rob@Roberts-MBP.fios-router.home>
2023-03-12 22:50:31 -0400

    add some more tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,5 +1,8 @@
 use seq_geom_parser::{FragmentGeomDesc, PiscemGeomDesc, SalmonSeparateGeomDesc};
 
+/// Parsing a simple format (10xV3 in this case) should work.
+/// We check this by ensuring that the format description makes the
+/// round trip through parsing and back through printing.
 #[test]
 fn test_parse_format_simple() {
     let arg = "1{b[16]u[12]x:}2{r:}";
@@ -13,6 +16,9 @@ fn test_parse_format_simple() {
     };
 }
 
+/// Parsing a complex format (sciseqv3 in this case) should work.
+/// We check this by ensuring that the format description makes the
+/// round trip through parsing and back through printing.
 #[test]
 fn test_parse_format_complex() {
     let arg = "1{b[9-10]f[ACCGT]u[12]b[10]}2{r:}";
@@ -26,6 +32,9 @@ fn test_parse_format_complex() {
     };
 }
 
+/// Parsing a simple format into a `PiscemGeomDesc` should work.
+/// We check this by ensuring that the format description makes the
+/// round trip through parsing and back through printing.
 #[test]
 fn test_parse_piscem_simple() {
     let arg = "1{b[16]u[12]x:}2{r:}";
@@ -70,8 +79,12 @@ fn test_parse_piscem_complex() {
     };
 }
 
+/// Parsing a simple format into a `PiscemGeomDesc` should work.
+/// We check this by ensuring that the format description makes the
+/// round trip through parsing  and ensure that it parsed as what
+/// we expect.
 #[test]
-fn test_salmon_piscem() {
+fn test_salmon_simple() {
     let arg = "1{b[16]u[12]x:}2{r:}";
     match FragmentGeomDesc::try_from(arg) {
         Ok(frag_desc) => {
@@ -95,9 +108,38 @@ fn test_salmon_piscem() {
     };
 }
 
+/// Parsing an invalid geometry description string should lead to
+/// an `Err` returned from the parser.
 #[test]
 fn test_fail_on_bad_geo() {
     let arg = "1{b[16]v[3]u[12]x:}2{r:}";
+    match FragmentGeomDesc::try_from(arg) {
+        Ok(_frag_desc) => {
+            panic!("this should not be parsed {}", arg);
+        }
+        Err(_e) => {}
+    };
+}
+
+/// Parsing a bad geometry (one that has valid tokens but doesn't follow
+/// the actual grammar) description string should lead to an `Err` returned
+/// from the parser.
+#[test]
+fn test_fail_on_ambig_geo() {
+    let arg = "1{b[16]u[12-13]x:}2{r:}";
+    match FragmentGeomDesc::try_from(arg) {
+        Ok(_frag_desc) => {
+            panic!("this should not be parsed {}", arg);
+        }
+        Err(_e) => {}
+    };
+}
+
+/// Parsing a bad geometry (a proper geometry followed by nonsense) 
+/// description string should lead to an `Err` returned from the parser.
+#[test]
+fn test_fail_on_superfluous_input() {
+    let arg = "1{b[16]u[12]x:}2{r:}_flargbarg";
     match FragmentGeomDesc::try_from(arg) {
         Ok(_frag_desc) => {
             panic!("this should not be parsed {}", arg);
